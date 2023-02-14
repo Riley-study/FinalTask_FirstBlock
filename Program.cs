c61ef654ab174dd09abfd9aadab667f676922306
Natalya <nataliettaa@mail.ru>
2023-02-14 23:47:35 +0500

    Add method CountElementsLessThenFour

--- Program.cs
+++ Program.cs
@@ -3,3 +3,35 @@
 
 using static System.Console;
 Clear();
+
+string[] InitArray = {"Hellow", "34", "1", "ghjk", "uuu"};
+
+int countLengthNewArray = CountElementsLessThenFour(InitArray);
+WriteLine(countLengthNewArray);
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////Methods/////////////////////////////////////////
+
+// Метод, считающий количество элементов в массиве, длинна которых строго меньше четырех
+
+int CountElementsLessThenFour(string[] inArray)
+{
+    int count = 0;
+    int lengthElement;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        lengthElement = inArray[i].Length;
+        if (lengthElement < 4) count++;
+    }
+    return count;
+}
